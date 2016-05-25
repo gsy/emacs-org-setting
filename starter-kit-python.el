bb4a2c1566f333b3245a855b31114416e579e49a
guang <gtcxg@hotmail.com>
2016-05-26 00:32:02 +0800

    restore

--- starter-kit-python.el
+++ starter-kit-python.el
@@ -8,3 +8,9 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 
 (setq jedi:complete-on-dot t)
+
+(defun annotate-pdb ()
+  (interactive)
+  (highlight-lines-matching-regexp "import ipdb")
+  (highlight-lines-matching-regexp "ipdb.set_trace()"))
+(add-hook 'python-mode-hook 'annotate-pdb)
